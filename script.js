1b94dedb34532ea59c00895b304252552bd1532d
Yasmin da Silva <95134741+yaahsilva16@users.noreply.github.com>
2022-01-20 15:25:56 -0300

    ENTREGANDO MEU FOR/WHILE

---
+++ script.js
@@ -0,0 +1,17 @@
+for (let i = 1; i<11; i++){
+    console.log(i)
+}
+
+let a = 10 
+while(a >=1){
+    console.log(a) 
+    a--
+}
+
+for (let i = 1; i <= 100; i += 2){
+    console.log(i);
+}
+
+for (let i = 0; i <= 100; i += 2){
+    console.log(i);
+}
